c6fcf7c105e52cd538d62cad06eb693d5f9dc6fa
Heidi Liang <yhliang@hpc-login-1-2.local>
2019-01-21 23:42:17 -0800

    add explanations

--- lab2.py
+++ lab2.py
@@ -1,23 +1,28 @@
+#unzip file
 import tarfile
 tf = tarfile.open('/Users/HEIDI/Documents/19advinformatics/lab2/lab2data.tar.gz')
 tf.extractall()
 
+#make lists for input and output
 import glob
-import re
-
 input = glob.glob('/lab2data/data/*')
 output = glob.glob('/lab2data/output/*')
 
+#select out the variable that distinguishes each file (.# and .a/b)
+#join the # and a or b together 
+import re
 f = lambda x: ''.join(re.findall('datafile\.(.*)\.(.*)\..*',x)[0])
-
 data_input = map(f, input)
 f = lambda x: ''.join(re.findall('outfile\.(.*)\.(.*)\..*',x)[0])
 data_output = map(f, output)
 
+#find out those missing in the output folder
 missing = set(data_input).difference(data_output)
 
+#number of missing files
 print(len(missing))
 
+#write out the names of the missing files in "missing.txt"
 with open('missing.txt','w') as f:
  for x in missing:
   f.write('outfile.%s.%s.out\n' % (x[:-1],x[-1]))
